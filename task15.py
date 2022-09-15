3455d252e5cf89e3034bfb7978852b994623877a
lin6echo <bajzathcsaba@gmail.com>
2022-09-15 17:40:05 +0200

    1

--- task15.py
+++ task15.py
@@ -7,22 +7,8 @@ def day_number():
      for x in range(0, c):
           if i == 6: 
                i = 0
-               i = i +1
                
           e.append(d[x])
           print(e)
           
 day_number()
-
-
-
-# def day_name():
-#      d = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
-#      for x in d:
-#           print(x)
-     
-# day_name()          
-
-
-
-
