683755d99fba0474447dacd0d9a12dd0cf1782d7
lin6echo <bajzathcsaba@gmail.com>
2022-09-16 20:55:20 +0200

    1

--- task15.py
+++ task15.py
@@ -1,9 +1,8 @@
+d = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+c = 366
+e = []
+
 def day_name():
-    global c
-    c = 366
-    global d
-    d = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    e = []
     i = 0 
     for x in range(0, c):
         if i == 7:
@@ -12,12 +11,10 @@ def day_name():
         i += 1
     print(e)
     
-          
 day_name()
 
 def day_number():
-    for x in range(0, c):
+    for x in range(1, c):
         print(x)
     
-          
 day_number()
