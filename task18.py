414f7598a8724564d7e86eb821283ef8fbcca39d
lin6echo <bajzathcsaba@gmail.com>
2022-09-17 13:28:05 +0200

    1

--- task18.py
+++ task18.py
@@ -1,3 +1,38 @@
-january = 31
+january = 32
+print(list(range(1, january+1)))
 
-print(january[1,31])
+february = 28
+print(list(range(1, february)))
+
+march = 31
+print(list(range(1, march)))
+
+april = 30
+print(list(range(1, april)))
+
+may = 31
+print(list(range(1, may)))
+
+june = 30
+print(list(range(1, june)))
+
+july = 31
+print(list(range(1, july)))
+
+august =31
+print(list(range(1, august)))
+
+september = 30
+print(list(range(1, september)))
+
+october = 31
+print(list(range(1, october)))
+
+november = 30
+print(list(range(1, november)))
+
+december = 31
+print(list(range(1, december)))
+
+year = january + february + march + april + may + june + july + august + september + october + november + december
+print(year)
