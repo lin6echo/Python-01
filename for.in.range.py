ec2de3ed8300a362548f593b4bd01d7c9ed30749
lin6echo <bajzathcsaba@gmail.com>
2022-09-25 14:20:25 +0200

    1

---
+++ for.in.range.py
@@ -0,0 +1,19 @@
+
+print(list(i for i in range(10)))
+print(list(1 for i in range(10)))
+
+for i in range(10):
+    print(i)
+    
+x = list(range(10))
+print(x)
+
+i = 0
+while i < len(range(5, 7)):
+    print(i)
+    i += 1
+    
+i = 0
+while i < 5:
+    print(i)
+    i += 1
