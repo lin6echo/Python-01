2c33dde3ca22351961e379d63633ba3a954f76cb
lin6echo <lin6echo@gmail.com>
2022-10-01 15:59:32 +0200

    1

---
+++ iteratewithenumerate.py
@@ -0,0 +1,14 @@
+data = [1, 2, -4, -3]
+for i in range(len(data)):
+    if data[i] < 0:
+        data[i] = 0
+
+print(data)
+
+data = [1, 2, -4, -3]
+for idx, num in enumerate(data):
+    if num < 0:
+        data[idx] = 0
+
+print(data)
+
